ec6425f7abd5b07518ca4eba39250de5ebbf2a12
Amol Gangadhare <amol.gangadhare@centelon.com>
2019-03-08 14:14:37 +0530

    Removed permissions in FlutterBarcodeScannerPlugin

--- android/src/main/java/com/amolg/flutterbarcodescanner/FlutterBarcodeScannerPlugin.java
+++ android/src/main/java/com/amolg/flutterbarcodescanner/FlutterBarcodeScannerPlugin.java
@@ -26,7 +26,7 @@ import io.flutter.plugin.common.PluginRegistry.ActivityResultListener;
 /**
  * FlutterBarcodeScannerPlugin
  */
-public class FlutterBarcodeScannerPlugin implements MethodCallHandler, ActivityResultListener, PluginRegistry.RequestPermissionsResultListener {
+public class FlutterBarcodeScannerPlugin implements MethodCallHandler, ActivityResultListener{
     private static final String CHANNEL = "flutter_barcode_scanner";
     private static final int REQUEST_CODE_CAMERA_PERMISSION = 3777;
     private static FlutterBarcodeScannerPlugin instance;
@@ -52,7 +52,6 @@ public class FlutterBarcodeScannerPlugin implements MethodCallHandler, ActivityR
             final MethodChannel channel = new MethodChannel(registrar.messenger(), CHANNEL);
             instance = new FlutterBarcodeScannerPlugin((FlutterActivity) registrar.activity());
             registrar.addActivityResultListener(instance);
-            registrar.addRequestPermissionsResultListener(instance);
             channel.setMethodCallHandler(instance);
         }
     }
@@ -67,57 +66,13 @@ public class FlutterBarcodeScannerPlugin implements MethodCallHandler, ActivityR
                     throw new IllegalArgumentException("Plugin not passing a map as parameter: " + call.arguments);
                 }
                 arguments = (Map<String, Object>) call.arguments;
-                boolean handlePermission = (boolean) arguments.get("handlePermissions");
-                this.executeAfterPermissionGranted = (boolean) arguments.get("executeAfterPermissionGranted");
-
-                if (checkSelfPermission(activity,
-                        Manifest.permission.CAMERA)
-                        != PackageManager.PERMISSION_GRANTED) {
-                    if (shouldShowRequestPermissionRationale(activity,
-                            Manifest.permission.CAMERA)) {
-                        if (handlePermission) {
-                            requestPermissions();
-                        } else {
-                            setNoPermissionsError();
-                        }
-                    } else {
-                        if (handlePermission) {
-                            requestPermissions();
-                        } else {
-                            setNoPermissionsError();
-                        }
-                    }
-                } else {
                     lineColor = (String) arguments.get("lineColor");
                     startBarcodeScannerActivityView();
-                }
             }
         } catch (Exception e) {
             Log.e(TAG, "onMethodCall: " + e.getLocalizedMessage());
         }
     }
-
-
-    @TargetApi(Build.VERSION_CODES.M)
-    private void requestPermissions() {
-        activity.requestPermissions(new String[]{Manifest.permission.CAMERA}, REQUEST_CODE_CAMERA_PERMISSION);
-    }
-
-    private boolean shouldShowRequestPermissionRationale(Activity activity,
-                                                         String permission) {
-        if (Build.VERSION.SDK_INT >= 23) {
-            return activity.shouldShowRequestPermissionRationale(permission);
-        }
-        return false;
-    }
-
-    private int checkSelfPermission(Context context, String permission) {
-        if (permission == null) {
-            throw new IllegalArgumentException("permission is null");
-        }
-        return context.checkPermission(permission, android.os.Process.myPid(), Process.myUid());
-    }
-
 
     private void startBarcodeScannerActivityView() {
         try {
@@ -128,33 +83,6 @@ public class FlutterBarcodeScannerPlugin implements MethodCallHandler, ActivityR
         }
     }
 
-
-    @Override
-    public boolean onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        if (requestCode == REQUEST_CODE_CAMERA_PERMISSION) {
-            for (int i = 0; i < permissions.length; i++) {
-                String permission = permissions[i];
-                int grantResult = grantResults[i];
-
-                if (permission.equals(Manifest.permission.CAMERA)) {
-                    if (grantResult == PackageManager.PERMISSION_GRANTED) {
-                        if (executeAfterPermissionGranted) {
-                            startBarcodeScannerActivityView();
-                        }
-                    } else {
-                        setNoPermissionsError();
-                    }
-                }
-            }
-        }
-        return false;
-    }
-
-    private void setNoPermissionsError() {
-        pendingResult.error("permission", "you don't have the user permission to access the camera", null);
-        pendingResult = null;
-        arguments = null;
-    }
 
     /**
      * Get the barcode scanning results in onActivityResult
--- lib/flutter_barcode_scanner.dart
+++ lib/flutter_barcode_scanner.dart
@@ -2,29 +2,14 @@ import 'dart:async';
 
 import 'package:flutter/services.dart';
 
-bool _handlePermissions = true;
-bool _executeAfterPermissionGranted = true;
 
 class FlutterBarcodeScanner {
   static const MethodChannel _channel =
       const MethodChannel('flutter_barcode_scanner');
 
-  FlutterBarcodeScanner setHandlePermissions(bool handlePermissions) {
-    _handlePermissions = handlePermissions;
-    return this;
-  }
-
-  FlutterBarcodeScanner setExecuteAfterPermissionGranted(
-      bool executeAfterPermissionGranted) {
-    _executeAfterPermissionGranted = executeAfterPermissionGranted;
-    return this;
-  }
-
   //Scan barcode and return the barcode string
   static Future<String>  scanBarcode(String lineColor) async {
     Map params = <String, dynamic>{
-      "handlePermissions": _handlePermissions,
-      "executeAfterPermissionGranted": _executeAfterPermissionGranted,
       "lineColor":lineColor
     };
     String barcodeResult = await _channel.invokeMethod('scanBarcode', params);
