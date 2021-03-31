4b82893eb218e48e82a38cd229dc48fae3edbcd8
stchoupkoua <simplice.tchoupkoua@parcaune.com>
2021-03-31 22:48:13 +0200

    refactoring

--- example/android/app/src/main/java/com/amolg/flutterbarcodescannerexample/EmbeddingV1Activity.java
+++ example/android/app/src/main/java/com/amolg/flutterbarcodescannerexample/EmbeddingV1Activity.java
@@ -1,11 +1,11 @@
 package com.amolg.flutterbarcodescannerexample;
 
 import android.os.Bundle;
-import io.flutter.app.FlutterActivity;
+import io.flutter.app.FlutterFragmentActivity;
 import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
-public class EmbeddingV1Activity extends FlutterActivity {
+public class EmbeddingV1Activity extends FlutterFragmentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- example/android/app/src/main/java/com/amolg/flutterbarcodescannerexample/MainActivity.java
+++ example/android/app/src/main/java/com/amolg/flutterbarcodescannerexample/MainActivity.java
@@ -1,8 +1,8 @@
 package com.amolg.flutterbarcodescannerexample;
 
-import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.android.FlutterFragmentActivity;
 
-public class MainActivity extends FlutterActivity {
+public class MainActivity extends FlutterFragmentActivity {
   // You can keep this empty class or remove it. Plugins on the new embedding
   // now automatically registers plugins.
 }
