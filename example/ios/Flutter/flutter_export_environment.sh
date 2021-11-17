e6bc35231b72fe8dc194fe32babcdfdcdfd9176e
Abdul Manaf P Fassal <manaf@mmgc-india.org>
2021-11-17 12:15:10 +0530

    cancel button removed

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Volumes/MyData/Flutter_SDK/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/admin/Desktop/FlutterBarcodeScanner/flutter_barcode_scanner/example"
+export "FLUTTER_ROOT=/home/cordova/flutter_linux_2.5.3-stable/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/cordova/cordova_projects/flutter_barcode_scanner/example"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_BUILD_NAME=2.0.0.0"
 export "FLUTTER_BUILD_NUMBER=2.0.0.0"
 export "DART_OBFUSCATION=false"
